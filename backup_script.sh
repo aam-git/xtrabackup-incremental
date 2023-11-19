2d8dc7c4a839c363ccaa0b7acec2690229a5e038
AAMServices <info@aamservices.uk>
2023-11-19 16:31:33 +0000

    fix incremental 3

--- backup_script.sh
+++ backup_script.sh
@@ -1,68 +1,83 @@
 #!/bin/bash
 while true; do
+    # Backup configuration
+    MYSQL_USER=${MYSQL_USER}
+    MYSQL_PASSWORD=${MYSQL_PASSWORD}
+    MYSQL_HOST=${MYSQL_HOST}
+    DATABASES=${DATABASES}
 
-	# Backup configuration
-	MYSQL_USER=${MYSQL_USER}
-	MYSQL_PASSWORD=${MYSQL_PASSWORD}
-	MYSQL_HOST=${MYSQL_HOST}
-	DATABASES=${DATABASES}
+    BASE_DIR=${BASE_DIR}
+    INCREMENTAL_DIR=${INCREMENTAL_DIR}
+    BACKUP_RETENTION=${BACKUP_RETENTION:-"30 days"}
+    ARCHIVE_DIR=${ARCHIVE_DIR}  # Set your archive directory
+    
+    INTERVAL=${INTERVAL:-3600}
 
-	BASE_DIR=${BASE_DIR}
-	INCREMENTAL_DIR=${INCREMENTAL_DIR}
-	BACKUP_RETENTION=${BACKUP_RETENTION:-"30 days"}
-	ARCHIVE_DIR=${ARCHIVE_DIR}  # Set your archive directory
-	
-	INTERVAL=${INTERVAL:-3600}
+    REQUIRED_VARS=("MYSQL_USER" "MYSQL_PASSWORD" "MYSQL_HOST" "BASE_DIR" "INCREMENTAL_DIR" "DATABASES" "ARCHIVE_DIR"  "INTERVAL")
 
-	REQUIRED_VARS=("MYSQL_USER" "MYSQL_PASSWORD" "MYSQL_HOST" "BASE_DIR" "INCREMENTAL_DIR" "DATABASES" "ARCHIVE_DIR"  "INTERVAL")
+    for var in "${REQUIRED_VARS[@]}"; do
+        if [ -z "${!var}" ]; then
+            echo "Error: Environment variable $var is not set."
+            exit 1
+        fi
+    done
 
-	for var in "${REQUIRED_VARS[@]}"; do
-		if [ -z "${!var}" ]; then
-			echo "Error: Environment variable $var is not set."
-			exit 1
-		fi
-	done
+    # Function to archive backups and delete old archives
+    archive_backups() {
+        mkdir -p "$ARCHIVE_DIR"
+        ARCHIVE_NAME="${ARCHIVE_DIR}/backup_$(date +%Y%m%d).tar.gz"
+        echo "Archiving previous backups..."
+        tar -czvf "$ARCHIVE_NAME" "$BASE_DIR" "$INCREMENTAL_DIR"*
+        rm -rf "$BASE_DIR" "$INCREMENTAL_DIR"*
+        date +%s > "${BASE_DIR}/last_archive_timestamp"
 
-	# Function to archive backups and delete old archives
-	archive_backups() {
-		mkdir -p "$ARCHIVE_DIR"
-		ARCHIVE_NAME="${ARCHIVE_DIR}/backup_$(date +%Y%m%d).tar.gz"
-		echo "Archiving previous backups..."
-		tar -czvf "$ARCHIVE_NAME" "$BASE_DIR" "$INCREMENTAL_DIR"*
-		rm -rf "$BASE_DIR" "$INCREMENTAL_DIR"*
-		date +%s > "${BASE_DIR}/last_archive_timestamp"
+        # Delete archives older than retention period
+        find "$ARCHIVE_DIR" -name 'backup_*.tar.gz' -mtime +$(( $(date -d "$BACKUP_RETENTION" +%s) / 86400 )) -exec rm {} \;
+    }
 
-		# Delete archives older than retention period
-		find "$ARCHIVE_DIR" -name 'backup_*.tar.gz' -mtime +$(( $(date -d "$BACKUP_RETENTION" +%s) / 86400 )) -exec rm {} \;
-	}
+    # Function to get the base directory for the next incremental backup
+    get_incremental_base_dir() {
+        if [ -f "${INCREMENTAL_DIR}/latest_increment" ]; then
+            cat "${INCREMENTAL_DIR}/latest_increment"
+        else
+            echo "$BASE_DIR"
+        fi
+    }
 
-	CURRENT_TIME=$(date +%s)
+    # Function to update the latest incremental backup reference
+    update_latest_increment() {
+        echo "$1" > "${INCREMENTAL_DIR}/latest_increment"
+    }
 
-	# Check if it's time to archive (24 hours = 86400 seconds)
-	if [ -f "$ARCHIVE_TIMESTAMP_FILE" ]; then
-		LAST_ARCHIVE_TIME=$(cat "$ARCHIVE_TIMESTAMP_FILE")
-		if [ -z "$LAST_ARCHIVE_TIME" ]; then
-			LAST_ARCHIVE_TIME="$CURRENT_TIME"
-		fi
-	else
-		LAST_ARCHIVE_TIME="$CURRENT_TIME"
-	fi
+    CURRENT_TIME=$(date +%s)
 
-	if [ $(($CURRENT_TIME - $LAST_ARCHIVE_TIME)) -ge 86400 ]; then
-		archive_backups
-	fi
+    # Check if it's time to archive
+    if [ -f "${BASE_DIR}/last_archive_timestamp" ]; then
+        LAST_ARCHIVE_TIME=$(cat "${BASE_DIR}/last_archive_timestamp")
+    else
+        LAST_ARCHIVE_TIME=0
+    fi
 
-	# Check if base backup needs to be created
-	if [ ! -d "$BASE_DIR" ]; then
-		echo "Creating base backup..."
-		xtrabackup --backup --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --target-dir=$BASE_DIR --databases="$DATABASES"
-	else
-		# Create incremental backup
-		NEXT_INC="$BASE_DIR" "${INCREMENTAL_DIR}$(date +%Y%m%d%H%M%S)"
-		echo "Creating incremental backup..."
-		xtrabackup --backup --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --target-dir=$NEXT_INC --incremental-basedir=$BASE_DIR --databases="$DATABASES"
-	fi
-	
-    # Wait for 1 hour (3600 seconds) before the next run
-    sleep 3600
+    if [ $((CURRENT_TIME - LAST_ARCHIVE_TIME)) -ge 86400 ]; then
+        archive_backups
+    fi
+
+    # Check if base backup needs to be created
+    if [ ! -d "$BASE_DIR" ] || [ ! -f "${BASE_DIR}/xtrabackup_info" ]; then
+        echo "Creating base backup..."
+        xtrabackup --backup --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --target-dir=$BASE_DIR --databases="$DATABASES"
+        # Reset the latest incremental backup reference
+        rm -f "${INCREMENTAL_DIR}/latest_increment"
+    else
+        # Determine the base directory for the next incremental backup
+        INCREMENTAL_BASE=$(get_incremental_base_dir)
+        NEXT_INC="${INCREMENTAL_DIR}/inc_$(date +%Y%m%d%H%M%S)"
+        mkdir -p "$NEXT_INC"
+        echo "Creating incremental backup based on $INCREMENTAL_BASE..."
+        xtrabackup --backup --host=$MYSQL_HOST --user=$MYSQL_USER --password=$MYSQL_PASSWORD --target-dir="$NEXT_INC" --incremental-basedir="$INCREMENTAL_BASE" --databases="$DATABASES"
+        update_latest_increment "$NEXT_INC"
+    fi
+
+    # Wait for the next run
+    sleep $INTERVAL
 done
